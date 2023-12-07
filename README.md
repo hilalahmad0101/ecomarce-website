96c7682a2315a18ed08be8e96da8ccf5077197bb
tauseedzaman <tauseedzamanofsorana@gmail.com>
2023-12-07 09:47:33 +0000

    admin profile bugs fixed

---
+++ README.md
@@ -0,0 +1,27 @@
+# Your Laravel Application Name
+
+## Description
+
+Briefly describe your Laravel application, including its purpose and key features.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+### Prerequisites
+
+Make sure you have the following installed:
+
+- [PHP](https://www.php.net/) (Version X.X or later)
+- [Composer](https://getcomposer.org/)
+
+### Clone the Repository
+
+```bash
+git clone https://github.com/your-username/your-laravel-app.git
--- resources/views/admin/auth/update-profile.blade.php
+++ resources/views/admin/auth/update-profile.blade.php
@@ -4,98 +4,138 @@
 @endsection
 @section('content')
     <div class="content">
-    <div class="page-inner">
+        <div class="page-inner">
 
-        <div class="container-fluid">
+            <div class="container-fluid">
 
-            <!-- Page Heading -->
-            <div class="card mb-4">
-                <div class="d-sm-flex align-items-center justify-content-between">
-                    <h3 class="mb-0 bc-title"><b>Update Profile</b></h3>
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a>
-                        </li>
-                        <li class="breadcrumb-item"><a href="#">Update Profile</a></li>
-                    </ol>
+                <!-- Page Heading -->
+                <div class="card mb-4">
+                    <div class="d-sm-flex align-items-center justify-content-between">
+                        <h3 class="mb-0 bc-title pl-3"><b>Update Profile</b></h3>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a>
+                            </li>
+                            <li class="breadcrumb-item"><a href="#">Update Profile</a></li>
+                        </ol>
+                    </div>
                 </div>
-            </div>
 
-            <div class="row">
+                <div class="row">
 
-                <div class="col-xl-12 col-lg-12 col-md-12">
+                    {{-- show error if available --}}
+                    @if (session('error'))
+                        <div class="col-md-12">
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>Error!</strong> {{ session('error') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                        aria-hidden="true">×</span></button>
+                            </div>
+                        </div>
+                    @endif
+                    {{-- show success message  --}}
+                    @if (session('success'))
+                        <div class="col-md-12">
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                <strong>Success!</strong> {{ session('success') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                        aria-hidden="true">×</span></button>
+                            </div>
+                        </div>
+                    @endif
 
-                    <div class="card o-hidden border-0 shadow-lg">
-                        <div class="card-body ">
-                            <!-- Nested Row within Card Body -->
-                            <div class="row justify-content-center">
-                                <div class="col-lg-8">
-                                    <div class="p-5">
-                                        <form class="admin-form"
-                                            action="{{ route('admin.update.profile') }}"
-                                            method="POST" enctype="multipart/form-data">
-@csrf
+                    <div class="col-xl-12 col-lg-12 col-md-12">
 
-                                            <div class="form-group">
-                                                <label for="name">Current Image</label>
-                                                <div class="col-lg-12 pb-1">
-                                                    <img class="admin-img"
-                                                        src="{{ asset('storage') }}/{{ Auth::guard('admin')->user()->image }}"
-                                                        alt="No Image Found">
+                        <div class="card o-hidden border-0 shadow-lg">
+                            <div class="card-body ">
+                                <!-- Nested Row within Card Body -->
+                                <div class="row justify-content-center">
+                                    <div class="col">
+                                        <div class="p-5">
+                                            <form class="admin-form" action="{{ route('admin.update.profile') }}"
+                                                method="POST" enctype="multipart/form-data">
+                                                @csrf
+
+                                                <div class="form-group">
+                                                    <label for="name">Current Image</label>
+                                                    <div class="col-lg-12 pb-1">
+                                                        <img class="admin-img"
+                                                            src="{{ asset('storage') }}/{{ Auth::guard('admin')->user()->image }}"
+                                                            alt="No Image Found">
+                                                    </div>
+                                                    <span>Image Size Should Be 40 x 40.</span>
                                                 </div>
-                                                <span>Image Size Should Be 40 x 40.</span>
-                                            </div>
 
-                                            <div class="form-group position-relative text-center">
-                                                <label class="file">
-                                                    <input type="file" accept="image/*" class="upload-photo"
-                                                        name="image" id="file" aria-label="File browser example">
-                                                    <span class="file-custom text-left">Upload Image...</span>
-                                                </label>
-                                                @error('image')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="name">User Name *</label>
-                                                <input type="text" name="username" class="form-control" id="name"
-                                                    placeholder="User Name" value="{{ Auth::guard('admin')->user()->username }}">
+                                                <div class="form-group position-relative text-center">
+                                                    <label class="file">
+                                                        <input type="file" accept="image/*" class="upload-photo"
+                                                            name="image" id="file" aria-label="File browser example">
+                                                        <span class="file-custom text-left">Upload Image...</span>
+                                                    </label>
+                                                    @error('image')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="name">Username *</label>
+                                                    <input type="text" name="username" class="form-control"
+                                                        id="name" placeholder="User Name"
+                                                        value="{{ Auth::guard('admin')->user()->username }}">
                                                     @error('username')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
-                                            </div>
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
 
 
-                                            <div class="form-group">
-                                                <label for="email">Email Address *</label>
-                                                <input type="email" name="email" class="form-control" id="email"
-                                                    placeholder="Email Address" value="{{ Auth::guard('admin')->user()->email }}">
+                                                <div class="form-group">
+                                                    <label for="email">Email Address *</label>
+                                                    <input type="email" name="email" class="form-control" id="email"
+                                                        placeholder="Email Address"
+                                                        value="{{ Auth::guard('admin')->user()->email }}">
                                                     @error('email')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
-                                            </div>
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
 
-                                            <div class="form-group">
-                                                <label for="phone">Phone Number *</label>
-                                                <input type="text" name="phone" class="form-control" id="phone"
-                                                    placeholder="Phone Number" value="{{ Auth::guard('admin')->user()->phone }}">
+                                                <div class="form-group">
+                                                    <label for="phone">Phone Number *</label>
+                                                    <input type="text" name="phone" class="form-control" id="phone"
+                                                        placeholder="Phone Number"
+                                                        value="{{ Auth::guard('admin')->user()->phone }}">
                                                     @error('phone')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
-                                            </div>
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
 
-                                            <div class="form-group">
-                                                <label for="phone">Password *</label>
-                                                <input type="password" name="password" class="form-control" id="password"
-                                                    placeholder="Enter Password" >
-                                                    
-                                            </div>
+                                                <div class="form-group">
+                                                    <label for="existingPassword">Existing Password *</label>
+                                                    <input type="password" name="existing_password" class="form-control"
+                                                        id="existingPassword" placeholder="Enter Existing Password">
+                                                </div>
 
+                                                <!-- New Password -->
+                                                <div class="form-group">
+                                                    <label for="newPassword">New Password *</label>
+                                                    <input type="password" name="password" class="form-control"
+                                                        id="newPassword" placeholder="Enter New Password">
+                                                    @error('password')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
 
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-secondary btn-block">Submit</button>
-                                            </div>
-                                            <div>
-                                        </form>
+                                                <!-- Password Confirmation -->
+                                                <div class="form-group">
+                                                    <label for="confirmPassword">Confirm Password *</label>
+                                                    <input type="password" name="password_confirmation" class="form-control"
+                                                        id="confirmPassword" placeholder="Confirm New Password">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <button type="submit"
+                                                        class="btn btn-secondary btn-block">Submit</button>
+                                                </div>
+                                                <div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -104,5 +144,4 @@
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
